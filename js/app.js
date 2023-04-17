faa1ae2febfcc343b1e2ca9d076679c447f3597f
xursand <xursand@>
2023-04-17 14:01:40 +0300

    lesson_commed

--- js/app.js
+++ js/app.js
@@ -21,3 +21,39 @@ for (let i = 0; i < asideDropdown.length; i++) {
         
     })
 }
+
+
+// =================================lesson===================
+
+let topic = document.querySelector('.lesson__card-topic')
+let readers = document.querySelector('.lesson__card-readers')
+let evaluation = document.querySelector('.lesson__card-evaluation')
+
+let lessonImg = document.querySelector('.lesson__img')
+let lessonComment = document.querySelector('.lesson__comment')
+let lessonCommentRetings = document.querySelector('.lesson__comment-retings')
+
+
+evaluation.addEventListener('click', function () {
+    lessonImg.classList.remove('active')
+    lessonComment.classList.remove('active')
+    lessonCommentRetings.classList.add('active')
+})
+
+
+readers.addEventListener('click', function() {
+lessonImg.classList.remove('active')
+lessonCommentRetings.classList.remove('active')
+lessonComment.classList.add('active')
+})
+
+
+
+topic.addEventListener('click',function() {
+    lessonComment.classList.remove('active')
+    lessonCommentRetings.classList.remove('active')
+    lessonImg.classList.add('active')
+})
+
+
+console.log(topic);
